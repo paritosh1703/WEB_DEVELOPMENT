0da9151602ab607c5142c97ec0efcdebd1d4c93f
Paritosh Rawat <66927815+paritosh1703@users.noreply.github.com>
2021-02-16 15:42:12 +0530

    deleting data from database

---
+++ Assignment 3/delete.php
@@ -0,0 +1,12 @@
+<?php 
+include('config.php');
+$id=$_GET['id'];
+$sql="DELETE FROM `users` WHERE id='$id' ";
+if(mysqli_query($conn,$sql)){
+	header("Location:changes.php");
+}
+else
+{
+	echo "Deletion failed...";
+}
+?>
